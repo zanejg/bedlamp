5cd4d5040add36246a73642d37347decef1a5e6b
Zane Gilmore <zane.gilmore@gmail.com>
2023-04-02 11:22:15 +1200

    First commit

---
+++ python/main.py
@@ -0,0 +1,12 @@
+import machine
+import time
+led = machine.Pin(5, machine.Pin.OUT)
+while True:
+	led.value(1)
+	time.sleep(1)
+	led.value(0)
+	time.sleep(1)
+	led.value(1)
+	time.sleep(0.5)
+	led.value(0)
+	time.sleep(0.5)
